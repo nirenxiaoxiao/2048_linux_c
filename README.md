8b24a5c684a7e11d9fb80849f30eb4a9fa8eae98
phil_hou <phil_hou@qq.com>
2014-04-09 14:59:57 +0800

    now can  back one step, add colors

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 ============
 
 2048 game written in C, run well in linux environment, with ncurses lib.
+
+use the command below to compile,
+gcc main.c -lncurses -static -g -o 2048.out
--- main.c
+++ main.c
@@ -13,6 +13,8 @@ int board[ WIDTH ][HEIGHT ];
 int boardOneBefore[WIDTH ][HEIGHT ];
 int boardTwoBefore[WIDTH ][HEIGHT ];
 
+int (*backTo)[WIDTH] = boardOneBefore;
+
 int leftMove();
 int rightMove();
 int upMove();
@@ -23,7 +25,22 @@ int randGenerate();
 int updateView();
 int winOrLose();
 int moveFlag;
+int printBoardBlackWhite();
+int printBoardWithColor();
+void storeNow();
+int log2_B(int value)
+{
+	int x = 0;
+	while(value > 1)
+	{
+		value>>=1;
+		x++;
+	}
+	return x;
+}
 
+typedef int printBoardFunc_T();
+printBoardFunc_T *printBoard;
 int updateView()
 {
 //	refresh();
@@ -61,12 +78,47 @@ int winOrLose()
 {
 	return CONTINUE;
 }
+
+int initBoard()
+{
+	int i,j;
+	for(i=0; i<HEIGHT; i++)
+		for(j=0; j<WIDTH; j++)
+			board[i][j]=0;	
+	randGenerate();
+	randGenerate();
+	backTo = boardOneBefore; 
+	storeNow();
+	return 0;
+}
+int initBoardColor()
+{
+	init_pair(1, COLOR_RED, COLOR_BLACK);
+	init_pair(2, COLOR_BLACK, COLOR_RED );
+	init_pair(3, COLOR_BLACK, COLOR_RED);
+	init_pair(4, COLOR_BLACK, COLOR_GREEN);
+	init_pair(5, COLOR_BLACK, COLOR_YELLOW);
+	init_pair(6, COLOR_BLACK, COLOR_BLUE);
+	init_pair(7, COLOR_BLACK, COLOR_MAGENTA);
+	init_pair(8, COLOR_BLACK, COLOR_CYAN);
+	init_pair(9, COLOR_BLACK, COLOR_WHITE);
+	init_pair(10, COLOR_BLACK, COLOR_WHITE);
+	init_pair(11, COLOR_BLACK, COLOR_WHITE);
+	init_pair(12, COLOR_BLACK, COLOR_WHITE);
+	return 0;
+}
 int initGame()
 {
 	moveFlag=0;
-	randGenerate();
-	randGenerate();
+	initBoard();
+	if(TRUE == has_colors())
+	{
+		printBoard= printBoardWithColor;	
+		initBoardColor();
+	}
+	else printBoard = printBoardBlackWhite;
 	updateView();
+	return 0;
 }
 
 
@@ -258,18 +310,49 @@ int downMove(){
 			}
 	}
 }
+void storeNow()
+{
+	int( *backNow )[WIDTH];
+	if(backTo == boardOneBefore )
+	{
+		backNow = boardTwoBefore;
+	}
+	else
+		backNow = boardOneBefore;
+	int i,j;
+	for(i=0; i < HEIGHT; i++)
+		for(j=0; j < WIDTH; j++)
+			backNow[i][j] = board[i][j];
+}
+void backMove()
+{
+	int i,j;
+	for( i = 0; i < HEIGHT; i++)
+		for( j = 0; j < WIDTH ; j++)
+			board[i][j] = backTo[i][j];
+	updateView();
+			
+}
 int moveDir(char dir)
 {
+	storeNow();
 	switch(dir)
 	{
 	case 'a': leftMove();	break;
 	case 'd': rightMove();	break;
 	case 's': downMove();	break;
 	case 'w': upMove();	break;
-	
+	case 'b': backMove();break;	
 	}	
+	if(moveFlag)
+	{
+		if(backTo == boardOneBefore)
+			backTo = boardTwoBefore;
+		else
+			backTo = boardOneBefore;	
+	}
 }
-int printBoard()
+int printBoardBlackWhite()
 {
 #ifdef _debug
 	int i, j;
@@ -277,21 +360,38 @@ int printBoard()
 		for(j = 0; j < HEIGHT; j++)
 	       	{
 			//printf("%8d", board[i][j]);
-			printw("%8d", board[i][j]);
+			printw("%4d  ", board[i][j]);
 		}
 		printw("\n");	
 	}
 #endif
 }
 
+int printBoardWithColor()
+{
+//	printw("printBOardWithColor\n");
+	int i, j;
+	for(i=0;i < WIDTH; i++){
+		for(j = 0; j < HEIGHT; j++)
+	    {
+			attron(COLOR_PAIR(log2_B( board[i][j] )+1));
+			printw("%4d  ", board[i][j]);
+			attroff(COLOR_PAIR(log2_B( board[i][j] )+1));
+		}
+		printw("\n");	
+	}
+}
+
 int main()
 {
 
 	initscr();
+	start_color();
+	noecho();
 	initGame();
 //	system("clear");
 	char ch;
-	ch=getchar();
+	ch=getch();
 	int ret; 
 	while(ch!='\x1b')
 	{
